c06596cfa9f4dffe3f03dd30f796f821fc5e7660
knknkn1162 <knknkn1162@gmail.com>
2020-05-01 23:59:56 +0900

    create base

--- lib/base.c
+++ lib/base.c
@@ -86,23 +86,3 @@ int fget_array(int *arr, int size) {
 #endif
     return 0;
 }
-
-// <size of arr> <arr[0]> <arr[1]> .. <arr[n-1]>
-int fget_array_with_size(int *arr) {
-#ifdef BUF_SIZE
-    char line[BUF_SIZE];
-    char *tmpbuf = line;
-    int i;
-    if(!fgets(line, BUF_SIZE, stdin)) return -1;
-    char *tmp = strtok(tmpbuf, " ");
-    arr[0] = strtol(tmp, NULL, 10);
-    tmpbuf = NULL;
-    for(i = 1; i <= arr[0]; i++) {
-        char *tmp = strtok(tmpbuf, " ");
-        arr[i] = strtol(tmp, NULL, 10);
-    }
-#else
-#error
-#endif
-    return 0;
-}
