a0fb2f927e3149ce642dfee9ee7279e89a43fea9
knknkn1162 <knknkn1162@gmail.com>
2020-05-04 23:16:15 +0900

    add library

---
+++ lib/graph.c
@@ -0,0 +1,26 @@
+
+#define VERTEX_MAX 100000
+#define EDGE_MAX 100000
+struct list {
+    struct list *next;
+    int dst;
+};
+
+struct list *create_graph(int vs) {
+    static struct list graph[VERTEX_MAX];
+    int i;
+    for(i = 0; i < vs; i++) {
+        graph[i].next = &graph[i];
+    }
+    return graph;
+}
+
+struct list lpool[EDGE_MAX];
+int lidx = 0;
+
+struct list *add_list(struct list *head, int v) {
+    struct list *elem = &lpool[lidx++];
+    elem->dst = v;
+    elem->next = head->next;
+    head->next = elem;
+}
---
+++ lib/ufind.c
@@ -0,0 +1,46 @@
+struct ufind {
+    int parent;
+    int depth;
+};
+
+struct ufind *create_ufind(int vs) {
+    struct ufind *ufind = malloc(vs * sizeof(struct ufind));
+    int i;
+    for(i = 0; i < vs; i++) {
+        ufind[i].parent = i;
+        ufind[i].depth = 0;
+    }
+    return ufind;
+}
+
+int find_root(struct ufind *ufind, int v) {
+    struct ufind *elem = &ufind[v];
+    int base = v;
+    while(base != elem->parent) {
+        base = elem->parent;
+        elem = &ufind[base];
+    }
+    return base;
+}
+
+int is_connect(struct ufind *ufind, int v1, int v2) {
+    int bv1 = find_root(ufind, v1);
+    int bv2 = find_root(ufind, v2);
+    return bv1 == bv2;
+}
+
+void unify(struct ufind *ufind, int v1, int v2) {
+    int bv1 = find_root(ufind, v1);
+    int bv2 = find_root(ufind, v2);
+    struct ufind *uv1 = &ufind[bv1];
+    struct ufind *uv2 = &ufind[bv2];
+    if(uv1->depth >= uv2->depth) {
+        if(uv1->depth == uv2->depth) uv1->depth++;
+        uv2->depth = uv1->depth;
+        uv2->parent = uv1->parent;
+    } else {
+        uv1->depth = uv2->depth;
+        uv1->parent = uv2->parent;
+    }
+    return;
+}
