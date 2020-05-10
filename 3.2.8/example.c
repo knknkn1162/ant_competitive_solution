9b9a924d97acf02f584e55d4047c513a297764c8
knknkn1162 <knknkn1162@gmail.com>
2020-05-10 11:10:04 +0900

    impl BFS

--- 3.2.8/example.c
+++ 3.2.8/example.c
@@ -33,6 +33,8 @@ struct range {
 
 #define LINE_MAX 500
 #define BOUNDARY_MAX (LINE_MAX*3)
+#define COMP_ROW_MAX BOUNDARY_MAX
+#define COMP_COL_MAX BOUNDARY_MAX
 
 void display(int row, int col, struct range *rs, struct range *cs, int size) {
 #define SROW_MAX 1000
@@ -101,6 +103,76 @@ struct point2d {
     int col;
 };
 
+enum type {
+    ENUM_PATH,
+    ENUM_WALL,
+};
+
+void create_map(char map[][COMP_COL_MAX+2], struct range *rs, struct range *cs, int size, int row, int col) {
+    int i, k1, k2;
+    for(i = 0; i <= row+1; i++) {
+        map[i][0] = map[i][col+1] = ENUM_WALL;
+    }
+    for(i = 0; i <= col+1; i++) {
+        map[0][i] = map[row+1][i] = ENUM_WALL;
+    }
+    for(i = 0; i < size; i++) {
+        struct range r = rs[i];
+        struct range c = cs[i];
+        for(k1 = r.start; k1 <= r.end; k1++) {
+            for(k2 = c.start; k2 <= c.end; k2++) {
+                map[k1][k2] = ENUM_WALL;
+            }
+        }
+    }
+    return;
+}
+
+struct point2d dist[4] = {
+    {0, 1},
+    {0, -1},
+    {1, 0},
+    {-1, 0},
+};
+
+#define QUEUE_MAX (COMP_COL_MAX*COMP_ROW_MAX)
+static struct point2d queue[QUEUE_MAX];
+static int qstart = 0;
+static int qend = 0;
+
+int is_empty(void) {
+    return qstart == qend;
+}
+void enqueue(struct point2d elem) {
+    queue[qstart++] = elem;
+}
+
+struct point2d dequeue(void) {
+    return queue[qend++];
+}
+
+void reset_queue() {
+    qstart = qend = 0;
+}
+
+void bfs(char map[][COMP_COL_MAX+2], struct point2d start) {
+    reset_queue();
+    enqueue(start);
+    int i;
+    while(!is_empty()) {
+        struct point2d p = dequeue();
+        map[p.row][p.col] = ENUM_WALL;
+        for(i = 0; i < 4; i++) {
+            struct point2d np = {
+                p.row + dist[i].row,
+                p.col + dist[i].col,
+            };
+            if(map[np.row][np.col] == ENUM_WALL) continue;
+            enqueue(np);
+        }
+    }
+}
+
 int main(void) {
     int row, col, ls;
     get_int3(&col, &row, &ls);
@@ -128,9 +200,32 @@ int main(void) {
 
     int comp_row = compress(rs, ls, row);
     int comp_col = compress(cs, ls, col);
+    static char map[COMP_ROW_MAX+2][COMP_COL_MAX+2];
+    create_map(map, rs, cs, ls, comp_row, comp_col);
 #ifdef DEBUG
     printf("after compression\n");
-    display(comp_row, comp_col, rs, cs, ls);
+    for(i = 1; i <= comp_row; i++) {
+        for(j = 1; j <= comp_col; j++) {
+            printf(" %d", map[i][j]);
+        }
+        putchar('\n');
+    }
 #endif
+    int count = 0;
+    for(i = 1; i <= comp_row; i++) {
+        for(j = 1; j <= comp_col; j++) {
+            // bfs
+            struct point2d start = {i, j};
+            if(map[i][j] == ENUM_PATH) {
+#ifdef DEBUG
+                printf("start: [%d, %d]\n", i, j);
+#endif
+                bfs(map, start);
+                count++;
+            }
+        }
+    }
+    printf("%d\n", count);
+
     return 0;
 }
