e15112dc5e7e60e4a6e6b1bb9137be1d31716657
knknkn1162 <knknkn1162@gmail.com>
2020-05-14 22:45:03 +0900

    [AC] 高橋君ゲーム

--- 2.3.4/arc057_b.c
+++ 2.3.4/arc057_b.c
@@ -5,78 +5,58 @@
 
 #define max(a,b) ((a) > (b) ? (a) : (b))
 #define min(a,b) ((a) > (b) ? (b) : (a))
-#define BUF_SIZE 20
-
-// size: specify sizeof(str)
-int get_str(char *str, int size) {
-    if(!fgets(str, size, stdin)) return -1;
-    return 0;
-}
-
-int get_int(void) {
-  int num;
-#ifdef BUF_SIZE
-  char line[BUF_SIZE];
-  if(!fgets(line, BUF_SIZE, stdin)) return 0;
-  sscanf(line, "%d", &num);
-#else
-#error
-#endif
-  return num;
-}
-
 int get_int2(int *a1, int *a2) {
-#ifdef BUF_SIZE
-  char line[BUF_SIZE];
-  if(!fgets(line, BUF_SIZE, stdin)) return -1;
-  sscanf(line, "%d %d", a1, a2);
-#else
-#error
-#endif
+  scanf("%d %d", a1, a2);
   return 0;
+}
+
+// <arr[0]> <arr[1]> .. <arr[size-1]>
+int fget_array(int *arr, int size) {
+    int i;
+    for(i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    return 0;
 }
 
 #define DAYS_MAX 2000
 
 int main(void) {
     int days, wins;
-    int i;
     get_int2(&days, &wins);
-    int games[DAYS_MAX+1];
-    int cum[DAYS_MAX+1];
-    for(i = 1; i <= days; i++) {
-        games[i] = get_int();
+    static int arr[DAYS_MAX];
+    fget_array(arr, days);
+    int sum = 0;
+    int i;
+    for(i = 0; i < days; i++) sum += arr[i];
+    if(sum == wins) {
+        printf("1\n");
+        return 0;
     }
 
-    // cum: sum of games in 1st~i-th days
-    for(i = 1; i <= days; i++) {
-        cum[i] = cum[i-1] + games[i];
-    }
-    static uint64_t dp[DAYS_MAX+1][DAYS_MAX+1];
-    memset(dp, 0x01, sizeof(uint64_t)*(DAYS_MAX+1)*(DAYS_MAX+1));
-    dp[0][0] = 0;
-    dp[1][0] = 0; dp[1][1] = 1;
+    // dp[days][wins] = min(K)
+    static int dp[DAYS_MAX+1][DAYS_MAX+1];
+    memset(dp, 0x7f, sizeof(int)*(DAYS_MAX+1)*(DAYS_MAX+1));
+    for(i = 0; i <= days; i++) dp[i][0] = 0;
+    dp[1][1] = 1;
 
-    // find smallest wins
-    int day, win;
-    for(day = 2; day <= days; day++) {
-        for(win = 1; win <= day; win++) {
-            // in lose case
-            dp[day][win] = min(dp[day][win], dp[day-1][win]);
-            // in win case
-            uint64_t min_wins = cum[day] * dp[day-1][win-1] / cum[day-1] + 1;
-            printf("%d %d -> min: %llu\n", day, win, min_wins);
-            dp[day][win] = min(dp[day][win], min_wins);
+    int d, w;
+    sum = arr[0];
+    for(d = 2; d <= days; d++) {
+        int cur = arr[d-1];
+        for(w = 1; w <= d; w++) {
+            int64_t llimit = (int64_t)(sum + cur) * dp[d-1][w-1] / sum + 1;
+            dp[d][w] = min(dp[d-1][w], llimit);
+        }
+        sum += cur;
+    }
+
+    for(i = days; i >= 0; i--) {
+        if(dp[days][i] <= wins) {
+            printf("%d\n", i);
+            return 0;
         }
     }
-    int ans = 0;
-    for(i = 1; i <= days; i++) {
-#ifdef DEBUG
-        printf("%d: %d\n", i, (int)dp[days][i]);
-#endif
-        if(dp[days][i] > (uint64_t)wins) break;
-        ans++;
-    }
-    printf("%d\n", ans);
+
     return 0;
 }
