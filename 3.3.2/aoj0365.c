81ef6824c56a7ba58454370bef5fb71e622d8f19
knknkn1162 <knknkn1162@gmail.com>
2020-05-28 18:09:36 +0900

    [WIP] Swapping Characters

---
+++ 3.3.2/aoj0365.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h> // uint64_t
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) > (b) ? (b) : (a))
+
+int get_int(void) {
+  int num;
+  scanf("%d", &num);
+  return num;
+}
+
+int get_str(char *str, int size)
+{
+    char format[100];
+    if (size == 0)
+        return 0;
+    snprintf(format, sizeof(format), "%%%ds", size);
+    scanf(format, str);
+    return 0;
+}
+#define STR_MAX 200000
+#define BUF_MAX (STR_MAX+50)
+
+int main(void) {
+    static char str[BUF_MAX];
+    get_str(str, BUF_MAX);
+    int num = get_int();
+    return 0;
+}
