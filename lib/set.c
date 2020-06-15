5a25c29033f78d1d290270dd4dccc917685e40ce
knknkn1162 <knknkn1162@gmail.com>
2020-06-15 14:31:13 +0900

    fix multiset

--- lib/set.c
+++ lib/set.c
@@ -1,97 +1,73 @@
+#include <stdlib.h>
 //ここから二分探索木
-typedef struct bt
-{
-	int value;
-	struct bt *left;
-	struct bt *right;
-}btree;
+struct btree {
+    int value;
+    struct btree *left;
+    struct btree *right;
+};
  
-//int btsize=0;
-btree *root;
-//root = NULL;
- 
-void insert(int value)
+void insert(struct btree *root, int value)
 {
-	btree *x,*y,*z;
-	z = (btree *)(malloc(sizeof(btree)));//新しい要素のメモリ確保
-	z->value=value;
-	z->left=NULL;
-	z->right=NULL;
-	
-	x=root; y=NULL;
-	while(x!=NULL)
-	{
-		y=x;
-		if(z->value < x->value)x = x->left;
-		else x = x->right;
-	}
-	if(y==NULL)root=z;
-	else if(z->value < y->value)y->left=z;
-	else y->right=z;
-	//printf("inserted %d\n",value);
+    struct btree *x,*y,*z;
+    z = (struct btree *)(malloc(sizeof(struct btree)));//新しい要素のメモリ確保
+    z->value=value;
+    z->left=NULL;
+    z->right=NULL;
+    
+    x=root; y=NULL;
+    while(x!=NULL)
+    {
+        y=x;
+        if(z->value < x->value)x = x->left;
+        else x = x->right;
+    }
+    if(y==NULL)root=z;
+    else if(z->value < y->value)y->left=z;
+    else y->right=z;
 }
  
-btree *del(btree *dstt, btree *t)
-{
-	btree *q;
-	if(t->right!=NULL)t->right = del(dstt,t->right);
-	else
-	{
-		dstt->value = t->value; 
-		q = t;
-		t = t->left;
-		free(q);
-	}
-	return(t);
+struct btree *_del(struct btree *dstt, struct btree *t) {
+    struct btree *q;
+    if(t->right!=NULL)t->right = _del(dstt,t->right);
+    else
+    {
+        dstt->value = t->value; 
+        q = t;
+        t = t->left;
+        free(q);
+    }
+    return(t);
 }
  
-void delete(int value)
+void delete(struct btree *root, int value)
 {
-	btree *x,*y,*z,*p;
-	x=root;
-	while(x!=NULL && x->value!=value)
-	{
-		p=x;//親になる
-		if(value < x->value)x = x->left;
-		else x = x->right;
-	}
-	//printf("%d %d\n",x->left,x->right);
-	y=x;//削除されるべき値を持ったノード
-	if(y->right==NULL)//右部分木がない
-	{
-		z = y;
-		if(y==root)root =y->left;
-		else if(value < p->value)p->left =y->left;
-		else p->right = y->left;
-		free(z);
-	}
-	else if(y->left==NULL)//左部分木がない
-	{
-		z = y;
-		if(y==root)root =y->right;
-		else if(value < p->value)p->left =y->right;
-		else p->right = y->right;
-		//printf("%d %d\n",y,*(y));
-		free(z);
-	}
-	else
-	{
-		y->left = del(y,y->left);//左部分木の最右節点をこの木の根に移動する
-	}
-	//printf("deleted %d\n",value);
+    struct btree *x,*y,*z,*p;
+    x=root;
+    while(x!=NULL && x->value!=value)
+    {
+        p=x;//親になる
+        if(value < x->value)x = x->left;
+        else x = x->right;
+    }
+    y=x;//削除されるべき値を持ったノード
+    if(y->right==NULL)//右部分木がない
+    {
+        z = y;
+        if(y==root)root =y->left;
+        else if(value < p->value)p->left =y->left;
+        else p->right = y->left;
+        free(z);
+    }
+    else if(y->left==NULL)//左部分木がない
+    {
+        z = y;
+        if(y==root)root =y->right;
+        else if(value < p->value)p->left =y->right;
+        else p->right = y->right;
+        free(z);
+    }
+    else
+    {
+        y->left = _del(y,y->left);//左部分木の最右節点をこの木の根に移動する
+    }
 }
- 
-int minvalue()
-{
-	btree *x,*y;
-	x=root;
-	while(x!=NULL)
-	{
-		y=x;
-		x=x->left;
-	}
-	if(x==root)return -1;
-	else return (y->value);
-}
- 
-//ここまで二分探索木
