5de6709e2072dd6ddeac59d0f72e9941e950c3e5
knknkn1162 <knknkn1162@gmail.com>
2020-07-02 13:12:36 +0900

    [AC] AGC 009 A - Multiple Array

---
+++ 1.4.2/agc009_a.cpp
@@ -0,0 +1,57 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#define _GLIBCXX_DEBUG // check []
+#define DIVISOR 1000000007
+
+using namespace std;
+typedef pair<int,int> pii;
+typedef pair<int64_t, int64_t> pII;
+
+template<typename T>
+void cins(vector<T>& arr) { for(T& e: arr) cin >> e; }
+
+#ifdef DEBUG
+#define debug(fmt, ...) \
+    printf("[debug: %s] " fmt, __func__, __VA_ARGS__)
+#define ps(arr) \
+    for(auto e: arr) cout << e << " "; \
+    cout << endl;
+#else
+#define debug(fmt, ...)
+#define ps(arr)
+#endif
+
+int main(void) {
+    int num;
+    cin >> num;
+    int64_t offset = 0;
+    vector<int64_t> arr(num);
+    vector<int64_t> brr(num);
+    for(int i = 0; i < num; i++) {
+        cin >> arr[i] >> brr[i];
+    }
+    for(int i = num-1; i >= 0; i--) {
+        int64_t a = arr[i] + offset;
+        int64_t b = brr[i];
+        a %= b;
+        if(a==0) continue;
+        offset += (b-a);
+    }
+    cout << offset << endl;
+    return 0;
+}
